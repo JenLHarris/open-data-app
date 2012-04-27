3b05e46c9a460cfe46bb1a9321381f573ebf5918
Jen <harr0475@algonquinlive.com>
2012-04-27 15:59:23 -0400

    adding thomas as a user

--- create-user.php
+++ create-user.php
@@ -3,8 +3,7 @@
 require_once 'includes/db.php';
 require_once 'includes/users.php';
 
-$email = 'harr0475@algonquincollege.com';
-$password = 'password';
+
 
 $email = 'bradlet@algonquincollege.com';
 $password = 'password';
