45d9f8388fbf7aac93a39fa019b8ea4029c8d9a1
Jen <harr0475@algonquinlive.com>
2012-04-27 14:50:36 -0400

    redone with Thomas back in

--- create-user.php
+++ create-user.php
@@ -6,4 +6,7 @@ require_once 'includes/users.php';
 $email = 'harr0475@algonquincollege.com';
 $password = 'password';
 
+$email = 'bradlet@algonquincollege.com';
+$password = 'password';
+
 user_create($db, $email, $password);
